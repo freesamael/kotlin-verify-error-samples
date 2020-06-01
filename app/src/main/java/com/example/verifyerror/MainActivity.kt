48cd5d10b764bc1f63ec2367099399042c6b0fd3
Samael Wang <freesamael@gmail.com>
2020-06-01 23:35:04 +0800

    Add another sample which causes VerifyError on pre-L devices

--- app/src/main/java/com/example/verifyerror/MainActivity.kt
+++ app/src/main/java/com/example/verifyerror/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.verifyerror
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +9,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onBtn1Click(view: View) {
         Sample.errorCapturingThrowable()
+    }
+
+    fun onBtn2Click(view: View) {
+        Sample.errorResumeWithDefaultParametersPreL()
     }
 }
--- app/src/main/java/com/example/verifyerror/Sample.kt
+++ app/src/main/java/com/example/verifyerror/Sample.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.random.Random
 
 object Sample {
     private const val TAG = "Sample"
@@ -30,4 +31,25 @@ object Sample {
             }
         }
     }
+
+    /**
+     * On Dalvik VM (i.e. pre-L devices), this generates
+     * W/dalvikvm( 3892): VFY: register1 v1 type 17, wanted 5
+     * W/dalvikvm( 3892): VFY:  rejecting call to Lcom/example/verifyerror/Foo;.<init> (ZIILkotlin/jvm/internal/DefaultConstructorMarker;)V
+     * W/dalvikvm( 3892): VFY:  rejecting opcode 0x70 at 0x0040
+     * W/dalvikvm( 3892): VFY:  rejected Lcom/example/verifyerror/Sample$errorResumeWithDefaultParameters$1;.invokeSuspend (Ljava/lang/Object;)Ljava/lang/Object;
+     * W/dalvikvm( 3892): Verifier rejected class Lcom/example/verifyerror/Sample$errorResumeWithDefaultParameters$1;
+     */
+    fun errorResumeWithDefaultParametersPreL() {
+        GlobalScope.launch {
+            val foo = Foo(baz = randomInt())
+            Log.d(TAG, "foo=$foo")
+        }
+    }
+
+    private suspend fun randomInt(): Int = withContext(Dispatchers.IO) {
+        return@withContext Random.nextInt()
+    }
 }
+
+class Foo(val bar: Boolean = false, val baz: Int = 0)
